9056c1c0e1aed117551d975fd3fc4cd8e6210107
rahil9 <rahiljethwa999@gmail.com>
2024-09-18 21:46:33 +0530

    progress-4

---
+++ src/app/api/check-unique-username/route.ts
@@ -0,0 +1,53 @@
+import dbConnect from "@/lib/dbConnect";
+import UserModel from "@/model/User";
+import { z } from "zod";
+import { usernameValidation } from "@/schemas/signUpSchema";
+import { ClientPageRoot } from "next/dist/client/components/client-page";
+
+const UsernameQuerySchema = z.object({username: usernameValidation})
+
+export async function GET(request:Request) {
+    await dbConnect()
+    try {
+        const {searchParams} = new URL(request.url)
+        // taking out username parameter from multiple other params
+        const queryParam = {
+            username: searchParams.get('Username')
+        }
+        // validating with zod
+        const result = UsernameQuerySchema.safeParse(queryParam)
+        console.log(result);
+
+        if (!result.success) {
+            // this would display errors related to username only and 
+            // if they aren't available then all other errors are shown indicated by []
+            // this is carried out using zod
+            const usernameError = result.error.format().username?._errors || []
+            return Response.json({
+                success: false,
+                message: usernameError?.length > 0 ? usernameError.join(', ') : 'Invalid query parameter'
+            }, {status: 400})
+        }
+
+        const {username} = result.data
+        const existingVerifiedUser = await UserModel.findOne({username, isVerified: true})
+        if (existingVerifiedUser) {
+            return Response.json({
+                success: false,
+                message: 'Username is already taken'
+            }, {status: 400})
+        }
+
+        return Response.json({
+            success: true,
+            message: 'Username is available'
+        }, {status: 400})
+
+    } catch (error) {
+        console.log('Error checking username', error)
+        return Response.json({
+            success: false,
+            message: 'Error checking username'
+        },{ status: 500 })
+    }
+}
